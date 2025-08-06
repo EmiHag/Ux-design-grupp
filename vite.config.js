bfa418998de9e091eb4a5ea6119b80ac652e53aa
EmiHag <emilia.haegglund@icloud.com>
2025-08-06 12:52:42 +0200

    Flyttade filer och uppdaterade projektstruktur

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// ⚠️ Ändra '/ditt-repo-namn/' till namnet på ditt GitHub-repo
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Ux-design-grupp/',
+});
